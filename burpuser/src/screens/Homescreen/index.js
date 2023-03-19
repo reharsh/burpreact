bd62467fdc0be2d030fe95bcf68aeadb1a1b9435
harsh <reharshmeena@gmail.com>
2023-03-19 19:56:56 +0530

    minor modification homescreen index.js

--- burpuser/src/screens/Homescreen/index.js
+++ burpuser/src/screens/Homescreen/index.js
@@ -1,6 +1,5 @@
 import { StyleSheet, FlatList } from 'react-native';
 import Card from './src/components/Card';
-import Header from './src/components/Header';
 import cards from './assets/data/cards.json';
 
 export default function Homescreen() {
