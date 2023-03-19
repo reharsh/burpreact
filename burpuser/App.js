32ba7d1d21dcc0633931a35d8ef524ef03f391a3
harsh <reharshmeena@gmail.com>
2023-03-19 22:53:37 +0530

    fixed some bugs in homescreen

--- burpuser/App.js
+++ burpuser/App.js
@@ -7,6 +7,7 @@ import Homescreen from './src/screens/Homescreen';
 export default function App() {
   return (
       <SafeAreaView>
+        <Header/>
         <View style={styles.container}>
           <Homescreen/>
           <StatusBar style={"auto"}/>
--- burpuser/src/screens/Homescreen/index.js
+++ burpuser/src/screens/Homescreen/index.js
@@ -1,11 +1,16 @@
 import { StyleSheet, FlatList } from 'react-native';
-import Card from './src/components/Card';
-import cards from './assets/data/cards.json';
+import Card from '../../components/Card';
+import Header from '../../components/Header';
+import cards from '../../../assets/data/cards.json';
+import { View } from 'react-native';
 
 export default function Homescreen() {
   return (
+    <View>
         <FlatList data={cards} renderItem={({item}) => <Card card={item}/>}
           showsVerticalScrollIndicator={false}/>
+          <Header/>
+    </View>
   );
 }
 
